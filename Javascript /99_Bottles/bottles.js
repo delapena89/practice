8f0fc1558443a1a06ef4c73f76d444e653f9a439
Luis <delapenl@gmail.com>
2015-07-17 10:18:05 -0600

    added 99 Bottles files

---
+++ Javascript /99_Bottles/bottles.js
@@ -0,0 +1,6 @@
+var totalBottles = 99;
+while (totalBottles > 0) {
+  console.log(totalBottles + " bottles of beer on the wall! ");
+  console.log("Take one down, pass it around, " + (totalBottles-1) + " bottles of beer on the wall!");
+totalBottles--
+}
