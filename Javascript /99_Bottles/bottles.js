1f038b8a5d41313032ce4b8699bbe9364dc25ddd
Luis <delapenl@gmail.com>
2015-07-17 10:30:55 -0600

    99 bottles

--- Javascript /99_Bottles/bottles.js
+++ Javascript /99_Bottles/bottles.js
@@ -3,4 +3,4 @@ while (totalBottles > 0) {
   console.log(totalBottles + " bottles of beer on the wall! ");
   console.log("Take one down, pass it around, " + (totalBottles-1) + " bottles of beer on the wall!");
 totalBottles--
-}
+};
